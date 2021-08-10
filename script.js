1186b18c7907556aca7f74e2701d9be092a51c64
Niloy Rahman <76911230+srniloy@users.noreply.github.com>
2021-08-10 17:42:53 +0600

    10/08/21

--- script.js
+++ script.js
@@ -60,18 +60,18 @@ navLi.forEach((element,i) =>{
 
 
 
-const videoItems = document.querySelectorAll('.video-item');
-const allSlideContainer = document.querySelector('.all-slide-items');
+// const videoItems = document.querySelectorAll('.video-item');
+// const allSlideContainer = document.querySelector('.all-slide-items');
 
-videoScroll();
-function videoScroll(){
-    console.log(allSlideContainer.offsetWidth);
-    if(window.innerWidth <= 572){
-        videoItems.forEach((element)=>{
-            element.style.width = (allSlideContainer.offsetWidth / 2) - (15/2) + "px";
-        });
-    }
-}
+// videoScroll();
+// function videoScroll(){
+//     console.log(allSlideContainer.offsetWidth);
+//     if(window.innerWidth <= 572){
+//         videoItems.forEach((element)=>{
+//             element.style.width = (allSlideContainer.offsetWidth / 2) - (15/2) + "px";
+//         });
+//     }
+// }
 
 
 
@@ -82,78 +82,324 @@ function videoScroll(){
 
 // ===================== Video Section==========================
 
-const videoContainer = document.querySelectorAll('.all-slide-items');
-const vc = document.querySelectorAll('.slider-container');
-let mousePosition = false;
-let mainPointOfMouse;
-let scrolledValue = 0;
-let insPointOfMouse;
-let scrollLeft;
 
-videoContainer.forEach((item,i)=>{
-    item.addEventListener('mousedown',(e)=>{
-        e.preventDefault();
-        mousePosition = true;
-        mainPointOfMouse = e.pageX - item.offsetLeft;
-        scrollLeft = item.scrollLeft;
-        console.log(mainPointOfMouse);
-    });
-    item.addEventListener('mouseup',()=>{
-        mousePosition = false;
-        scrolledValue += insPointOfMouse;
 
-    });
-    item.addEventListener('mouseleave',()=>{
-        mousePosition = false;
+
+const sliderArea = document.querySelector('.slider-area');
+const leftBtn = document.querySelector('.slider-area .left-btn');
+const rightBtn = document.querySelector('.slider-area .right-btn');
+const sliderContainer = document.querySelector('.slide-container');
+const slideBox = document.querySelector('.slide-box');
+
+const slides = document.querySelectorAll('.video-item');
+const slideContents = document.querySelectorAll('.slide-content');
+const widthOfSliderArea = sliderArea.offsetWidth;
+const numberOfTotalSlides = slides.length;
+let displaySlide = 6;
+
+// Fixing widths ------>
+
+slides.forEach((element,i) =>{
+    element.style.width = widthOfSliderArea/displaySlide + "px";
+    slideContents[i].style.width = (widthOfSliderArea/6)-15 + "px";
+});
+
+// <-------------------
+
+
+
+// cloning of sliderBox ----->
+const cloneSlideBox = slideBox.cloneNode(true);
+
+cloneSlideBox.style.left = "-" + slideBox.offsetWidth + "px";
+cloneSlideBox.classList.add("cloned-slide-box");
+sliderContainer.appendChild(cloneSlideBox);
+
+// <-----------------
+
+
+
+
+
+
+
+
+
+
+
+const leftWidthOfSlider = sliderArea.offsetLeft;
+let clickedPositionXOfSlider;
+let instantMovingPositionXOfSlider;
+let dragedValueXOfSlider;
+let translatedValueXOfSlider = 0;
+let instantTranslatedValueXOfSlider;
+const XForChangesInSlideBox = (slides[0].offsetWidth *6.5); // 740
+const XForChangesInCloneSlideBox = (slides[0].offsetWidth * 6.5) + slideBox.offsetWidth; // 2960
+let cloneSliderPosition;
+let SliderPosition;
+
+let isdraging;
+let isDragingLeft;
+let isUp = true;
+let isLeave = true;
+let isStartPositioningSlideBox;
+let isStartPositioningcloneSlideBox;
+
+let doForSlideBoxRight;
+let doForCloneSlideBoxRight;
+let doForSlideBoxLeft;
+let doForCloneSlideBoxLeft;
+let slideNumberByButton = 0;
+
+
+//let autoAnimatingTimeInterval = setInterval(() => {rightBtnAction();}, 1500);
+
+
+sliderArea.addEventListener('mouseleave',()=>{
+    isdraging = false;
+    isLeave = true;
+    if(isUp == false){
+        upOrLeaveAction();
+        isUp = true;
+    }
+    //autoAnimatingTimeInterval = setInterval(() => {rightBtnAction();},1500);
+});
+sliderArea.addEventListener('mouseover',()=>{
+    //clearInterval(autoAnimatingTimeInterval);
+});
+sliderArea.addEventListener('mouseup',()=>{
+    isdraging = false;
+    if(isLeave == false){upOrLeaveAction(); isLeave = true;}
+    isUp = true;
+    
+});
+sliderArea.addEventListener('mousedown',(e)=>{
+    e.preventDefault();
+   isdraging = true;
+   clickedPositionXOfSlider = e.pageX - leftWidthOfSlider;
+});
+//window.addEventListener('dragenter')
+sliderArea.addEventListener('mousemove',(e)=>{
+    e.preventDefault();
+    if(isdraging){
         
-    });
+        dragedValueXOfSlider = (e.pageX-leftWidthOfSlider) - clickedPositionXOfSlider;
+        instantMovingPositionXOfSlider = e.pageX - leftWidthOfSlider;
+        instantTranslatedValueXOfSlider = translatedValueXOfSlider + dragedValueXOfSlider;
+        if(dragedValueXOfSlider<0){isDragingLeft = true;}
+        else if(dragedValueXOfSlider>0){isDragingLeft = false;}
+        slideBox.style.transition = `all 0ms`;
+        cloneSlideBox.style.transition = `all 0ms`;
+        AniOfSliding();
+        isUp = false;
+        isLeave = false;
+    }
     
-    item.addEventListener('mousemove',(e)=>{
-        e.preventDefault();
-        if(mousePosition){
-            
-            
-            insPointOfMouse = e.pageX - mainPointOfMouse;
-            console.log(insPointOfMouse);
-            item.style.transform = "translate(" + (scrolledValue+insPointOfMouse)+"px)";
-            
-
-    
-            
-        }
-    });
 });
 
 
+function upOrLeaveAction(){
+    translatedValueXOfSlider += dragedValueXOfSlider;
 
-// videoContainer.addEventListener('touchstart',(e)=>{
-//     mousePosition = true;
-//     let touches = e.changedTouches;
-//     mainPointOfMouse = touches[0].pageX - videoContainer.offsetLeft;
-//     scrollLeft = videoContainer.scrollLeft;
-// });
-// videoContainer.addEventListener('touchend',()=>{
-//     mousePosition = false;
-// });
-// videoContainer.addEventListener('touchleave',()=>{
-//     mousePosition = false;
+    let slideNumber;
+    if(dragedValueXOfSlider<0){
+        slideNumber = Math.floor(translatedValueXOfSlider/slides[0].offsetWidth);
+    }else{
+        slideNumber = Math.ceil(translatedValueXOfSlider/slides[0].offsetWidth);
+    }
     
-// });
+    translatedValueXOfSlider = slides[0].offsetWidth * slideNumber;
+    instantTranslatedValueXOfSlider = translatedValueXOfSlider;
+    slideNumberByButton = slideNumber;
+    setTimeout(() => {
+        slideBox.style.transition = `all 300ms`;
+        cloneSlideBox.style.transition = `all 300ms`;
+        //animationOfDots();
+        AniOfSliding();
+    }, 100);
+}
 
-// videoContainer.addEventListener('touchmove',(e)=>{
-//     if(mousePosition){
-//         e.preventDefault();
-//         let touches = e.changedTouches;
-//         const insPointOfMouse = touches[0].pageX - mainPointOfMouse;
-//         if(insPointOfMouse < 0){
-//             videoContainer.scrollLeft += (videoItems[0].offsetWidth+15);
-//         }
-//         else if(insPointOfMouse > 0){
-//             videoContainer.scrollLeft -= (videoItems[0].offsetWidth+15);
-//         }
-//     }
-// });
 
+
+
+
+rightBtn.addEventListener("click",()=>{
+    rightBtnAction();
+    
+});
+leftBtn.addEventListener("click",()=>{
+    leftBtnAction();
+    
+});
+
+
+function rightBtnAction(){
+    slideNumberByButton--;
+    translatedValueXOfSlider = slides[0].offsetWidth * slideNumberByButton;
+    instantTranslatedValueXOfSlider = translatedValueXOfSlider;
+    AniOfSliding();
+    //animationOfDots();
+}
+function leftBtnAction(){
+    slideNumberByButton++;
+    translatedValueXOfSlider = slides[0].offsetWidth * slideNumberByButton;
+    instantTranslatedValueXOfSlider = translatedValueXOfSlider;
+    AniOfSliding();
+    //animationOfDots();
+}
+
+
+
+
+
+
+
+
+
+// Changing Animation of slides -------------------------->
+
+
+
+function AniOfSliding(){
+    
+    
+    // reset ->
+
+    if(instantTranslatedValueXOfSlider >= (slideBox.offsetWidth * 2)){
+        translatedValueXOfSlider = 0;
+        slideNumberByButton = 0;
+
+    }
+    if(instantTranslatedValueXOfSlider <= (slideBox.offsetWidth * (-2))){
+        translatedValueXOfSlider = 0;
+        slideNumberByButton = 0;
+
+    }
+
+    // <-
+
+
+    // Right Slde Transition ->
+
+    
+    
+    if(((XForChangesInSlideBox * (-1)) <= instantTranslatedValueXOfSlider) 
+    && (0 >= instantTranslatedValueXOfSlider)){
+        doForCloneSlideBoxRight = true;
+        cloneSlideBox.style.transform = `translateX(${instantTranslatedValueXOfSlider}px)`;
+    }
+    else if((XForChangesInSlideBox * (-1)) > instantTranslatedValueXOfSlider){
+
+        cloneSliderPosition = (slideBox.offsetWidth*2) + instantTranslatedValueXOfSlider;
+        
+        if(doForCloneSlideBoxRight){
+            cloneSlideBox.style.zIndex = "-1";
+            setTimeout(()=>{
+                cloneSlideBox.style.transform = `translateX(${cloneSliderPosition}px)`;
+                setTimeout(() => {
+                    cloneSlideBox.style.zIndex = "1";
+                }, 300);
+            },40);
+            doForCloneSlideBoxRight = false;
+        }else{
+            cloneSlideBox.style.transform = `translateX(${cloneSliderPosition}px)`;
+        }
+    }
+
+
+    if(((XForChangesInCloneSlideBox * (-1)) <= instantTranslatedValueXOfSlider)
+    && (0 >= instantTranslatedValueXOfSlider)){
+        slideBox.style.transform = `translateX(${instantTranslatedValueXOfSlider}px)`;
+        doForSlideBoxRight = true;
+    }
+    else if((XForChangesInCloneSlideBox * (-1)) > instantTranslatedValueXOfSlider){
+        SliderPosition = (cloneSlideBox.offsetWidth*2) + instantTranslatedValueXOfSlider;
+
+        if(doForSlideBoxRight){
+
+            slideBox.style.zIndex = "-1";
+            setTimeout(()=>{
+                slideBox.style.transform = `translateX(${SliderPosition}px)`;
+                setTimeout(() => {
+                    slideBox.style.zIndex = "1";
+                }, 300);
+            },40);
+
+
+            doForSlideBoxRight = false;
+        }else{
+            slideBox.style.transform = `translateX(${SliderPosition}px)`;
+        }
+    }
+
+
+    // <-
+
+
+
+
+    // Left Side Transition ->
+    
+
+    if(((XForChangesInSlideBox * (1.2)) >= instantTranslatedValueXOfSlider)
+    && (0 <= instantTranslatedValueXOfSlider)){
+        slideBox.style.transform = `translateX(${instantTranslatedValueXOfSlider}px)`;
+        doForSlideBoxLeft = true;
+    }
+    else if((XForChangesInSlideBox * (1.2)) < instantTranslatedValueXOfSlider){
+
+            cloneSliderPosition = (slideBox.offsetWidth*(-2)) + instantTranslatedValueXOfSlider;
+            //cloneSlideBox.style.left = `${cloneSlidePosition}px`;
+    
+            if(doForSlideBoxLeft){
+                slideBox.style.zIndex = "-1";
+            setTimeout(()=>{
+                slideBox.style.transform = `translateX(${cloneSliderPosition}px)`;
+                setTimeout(() => {
+                    slideBox.style.zIndex = "1";
+                }, 300);
+            },40);
+                doForSlideBoxLeft = false;
+            }else{
+                slideBox.style.transform = `translateX(${cloneSliderPosition}px)`;
+            }
+        }
+
+
+
+    if(((XForChangesInCloneSlideBox * (1.15)) >= instantTranslatedValueXOfSlider) 
+    && (0 <= instantTranslatedValueXOfSlider)){
+        cloneSlideBox.style.transform = `translateX(${instantTranslatedValueXOfSlider}px)`;
+        doForCloneSlideBoxLeft = true;
+    }
+    else if((XForChangesInCloneSlideBox * (1.15)) < instantTranslatedValueXOfSlider){
+        SliderPosition = (slideBox.offsetWidth*(-2)) + instantTranslatedValueXOfSlider;
+        
+        if(doForCloneSlideBoxLeft){
+            cloneSlideBox.style.zIndex = "-1";
+            setTimeout(()=>{
+                cloneSlideBox.style.transform = `translateX(${SliderPosition}px)`;
+                setTimeout(() => {
+                    cloneSlideBox.style.zIndex = "1";
+                }, 300);
+            },40);
+            doForCloneSlideBoxLeft = false;
+        }else{
+            cloneSlideBox.style.transform = `translateX(${SliderPosition}px)`;
+        }
+    }
+
+    // <---
+
+
+
+
+}
+
+
+
+
+// <-----------------------------------------------------------------------
 
 
 
@@ -185,10 +431,10 @@ const typeContentArea = document.querySelector('.type-contents-area');
 typeContentArea.style.height = typeContent.offsetHeight + "px";
 
 
-window.addEventListener('resize',()=>{
-    typeContentArea.style.height = typeContent.offsetHeight + "px";
-    videoScroll();
-});
+// window.addEventListener('resize',()=>{
+//     typeContentArea.style.height = typeContent.offsetHeight + "px";
+//     videoScroll();
+// });
 
 // ===================== type by button click================
 
